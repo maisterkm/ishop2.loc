a630e4b8859d311d9abd895ed1b692de0f47e15e
Alexander Maister <maister.alexander@gmail.com>
2020-05-30 13:56:38 +0300

    Added controller classes and dispatch method in Router

--- config/routes.php
+++ config/routes.php
@@ -6,5 +6,5 @@ use ishop\Router;
 Router::add('^admin$', ['controler' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
 Router::add('^admin/?(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['prifix' => 'admin']);
 
-Router::add('^$', ['controler' => 'Main', 'action' => 'index']);
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
